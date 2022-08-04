a7f0fcdf21bfaab6bfa56c976f5daa9375e817bc
AlairtonSilva <ahads.pje22@uea.edu.br>
2022-08-04 13:10:17 -0400

    Atualização da caixa

--- Assets/Scripts/box.cs
+++ Assets/Scripts/box.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 
 public class box : MonoBehaviour
 {
-    
-    public GameObject anim;
     public GameObject myPrefab;
 
-
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.CompareTag("Player"))
-        {
-            anim.gameObject.GetComponent<Animator>().Play("PressBox");
-            StartCoroutine(crash());
-            
-        }
+        if (col.gameObject.CompareTag("Player"))
+            if (col.transform.position.y > gameObject.transform.position.y)
+            {
+                gameObject.GetComponent<Animator>().Play("PressBox");
+                StartCoroutine(crash());
+            }
     }
-    
+
     IEnumerator crash()
     {
-        
         yield return new WaitForSeconds(1f);
-        var boxCrashs = Instantiate(myPrefab,anim.transform.parent);
-        boxCrashs.transform.position =
-            new Vector3(anim.transform.position.x+1, anim.transform.position.y, anim.transform.position.z);
-        print(boxCrashs.transform.position);
-        Destroy(anim);
+        var boxCrashs = Instantiate(myPrefab,gameObject.transform.parent);
+        boxCrashs.transform.position = new Vector3(
+            gameObject.transform.position.x+1, 
+            gameObject.transform.position.y, 
+            gameObject.transform.position.z);
+        Destroy(gameObject);
     }
     
  
